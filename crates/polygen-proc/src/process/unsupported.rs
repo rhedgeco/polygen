40570b907caee0dd6e85da9055ca73ed7d6a4ec2
Ryan Hedgecock <rhedgeco@gmail.com>
2023-07-24 15:44:54 -0700

    add ability to turn off unsupported errors

--- crates/polygen-proc/src/process/unsupported.rs
+++ crates/polygen-proc/src/process/unsupported.rs
@@ -1,8 +1,10 @@
 use quote::{__private::TokenStream, quote};
 
 pub fn unsupported(item: &syn::Item) -> TokenStream {
-    quote! {
-        #item
-        compile_error!("This item is currently unsupported by polygen. Please submit an issue. Or even better, submit a PR! :)");
-    }
+    #[cfg(feature = "allow-unsupported")]
+    let support_assert = quote!();
+    #[cfg(not(feature = "allow-unsupported"))]
+    let support_assert = quote!(compile_error!("This item is currently unsupported by polygen. Please submit an issue. Or even better, submit a PR! :)"););
+
+    quote!( #item #support_assert )
 }
