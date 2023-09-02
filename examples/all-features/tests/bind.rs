11ea759c96c805cb8363290d888d0f3820587567
Ryan Hedgecock <rhedgeco@gmail.com>
2023-09-01 17:09:05 -0700

    export example to file

--- examples/all-features/tests/bind.rs
+++ examples/all-features/tests/bind.rs
@@ -1,15 +1,17 @@
+use std::fs;
+
 use all_features::test;
 use polygen::PolyBag;
 use polygen_csharp::PolygenCSharp;
 
 #[test]
 fn bind() {
-    let data = PolygenCSharp {
+    let csharp = PolygenCSharp {
         lib_name: format!("all_features"),
         namespace: format!("AllFeatures"),
         bag: PolyBag::new("Native").register_function::<test>(),
-    }
-    .generate();
+    };
 
-    panic!("\n{data}\n");
+    fs::create_dir_all("./target/polygen").unwrap();
+    fs::write("./target/polygen/AllFeatures.cs", csharp.generate()).unwrap();
 }
