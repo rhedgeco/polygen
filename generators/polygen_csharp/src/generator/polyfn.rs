01cec7a0ae4a8bc98573902fe7df5c462da58cac
Ryan Hedgecock <rhedgeco@gmail.com>
2023-09-11 09:25:08 -0700

    clarify csharp functions as `extern`

--- generators/polygen_csharp/src/generator/polyfn.rs
+++ generators/polygen_csharp/src/generator/polyfn.rs
@@ -10,7 +10,7 @@ pub fn render_function(lib_name: impl AsRef<str>, f: &PolyFn) -> String {
     let out_type = render_fn_type(f.params.output.as_ref());
     let doc = formatdoc! {"
         [DllImport(\"{lib_name}\", EntryPoint = \"{entry_point}\", CallingConvention = CallingConvention.Cdecl)]
-        public static {out_type} {name}(polygen-inner);"
+        public static extern {out_type} {name}(polygen-inner);"
     };
 
     let inner = utils::render_each(f.params.inputs.iter(), ", ", render_function_input);
